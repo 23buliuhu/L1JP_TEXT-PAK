0c489596c9a7165444b7b612a3dab0a79fb2636d
QQings & QQueens <gmqqueen@gmail.com>
2014-08-31 07:16:19 -0700

    Update Lineage_1.bat

--- Lineage_1.bat
+++ Lineage_1.bat
@@ -1,4 +1,5 @@
-cmd git pull
+"C:\Program Files (x86)\Git\bin\git.exe" daemon --reuseaddr --base-path=C:\git --export-all --verbose --enable=receive-pack
+git pull
 start key/key.jpg
 start lineage.exe
 exit
